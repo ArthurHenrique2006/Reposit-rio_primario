9ae7b409756915c5044696eebdcf79a9453ac769
Arthur Henrique <edenaldoicomon@gmail.com>
2023-01-24 07:39:26 -0300

    V9

--- Portifólio/Lista de compras/Index.js
+++ Portifólio/Lista de compras/Index.js
@@ -1,26 +1,18 @@
-function recuperar(){
-    while(localStorage -! 0){
-        
-    }
-}
-
 function add(){
     //Adicionar itens
     var n = document.getElementById('name')
     var p = document.getElementById('preço')
     var lista = document.getElementById('L')
-
+    var t = document.getElementById('total')
     //Definir valores
     var preço = Number(p.value)
     var nome = String(n.value)
-
+    var subtotal = Number(t.value)
     //Criar itens da lista
     var item = document.createElement('option')
-    item.text = `${nome}----${preço}R$`
-    
+    item.text = `${nome}....${preço}R$`
+    var total = subtotal + preço
     //Adicionar itens a lista
     lista.add(item)
-
-    //Salvar no local storage
-    localStorage.setItem(nome, preço)
+    t.text(total)
 }
