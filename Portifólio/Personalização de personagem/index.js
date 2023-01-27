dcacc7db3e651acae3926bde9ea0c7c944c1a5c3
Arthur Henrique <edenaldoicomon@gmail.com>
2023-01-27 20:26:45 -0300

    V10
    Adicionei um projeto novo que ainda não está finalizado, uma espécie de "personalizador de personagem"

---
+++ Portifólio/Personalização de personagem/index.js
@@ -0,0 +1,6 @@
+function mudar_camisaB(){
+    document.getElementById('fotocamisa').src="Camisa_Branca.jpeg"
+}
+function mudar_camisaP(){
+    document.getElementById('fotocamisa').src="Camisa_Preta.png"
+}
